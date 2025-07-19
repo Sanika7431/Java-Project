5c0441c884aa534238369379c0731c99b72b5680
Sanika7431 <sanikabhor769@gmail.com>
2025-07-19 16:39:11 +0530

    Add files via upload

---
+++ BS.java
@@ -0,0 +1,20 @@
+package Web;
+
+class TempratureConverter
+{
+	static void temp( float celsius)
+	{
+		System.out.println("Temp in celsius: "+ celsius +" C");
+		float fahrenheit = (celsius * (1.8f) +32);
+		System.out.println("Temp convert celsius to fahrenheit:" + fahrenheit + " F");
+		double Celsius = ((fahrenheit-32)*(0.55555f));
+		System.out.println("Temp convert fahrenheit  to celsius :" + Celsius +" C");
+	}
+}
+public class BS {
+
+	public static void main(String[] args)
+	{
+		TempratureConverter.temp(77);
+	}
+}
