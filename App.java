2af586897e17c798116ef5a33132c82342633c7e
Sanika7431 <sanikabhor769@gmail.com>
2025-07-25 20:11:59 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,15 @@
+package greek;
+
+public class App {
+
+	static int a;
+	
+	static {
+		System.out.println("App Starting....");
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("App Running....");
+	}
+}
