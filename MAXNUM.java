a42dfe409f55fc6a43a5b955003646f97a66f666
Sanika7431 <sanikabhor769@gmail.com>
2025-07-26 14:36:18 +0530

    Add files via upload

---
+++ MAXNUM.java
@@ -0,0 +1,32 @@
+package greek;
+
+import java.util.Scanner;
+
+
+public class MAXNUM {
+	
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter 1st number:");
+		int a = s.nextInt();
+		System.out.println("Enter 2nd number:");
+		int b= s.nextInt();
+		System.out.println("Enter 3rd number:");
+		int c = s.nextInt();
+        int max ;
+		
+		if (a >= b && a>=c )
+		{
+			max=a;
+		}else if(b>=a && b>=c )
+		{
+			max=b;
+		}else {
+			System.out.println(max=b);
+		}
+		System.out.println("The largest Number :" +max );
+		
+	    s.close();
+	}
+}
