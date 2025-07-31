2c4ed22bd8789c3af31c3d7bb0a15f499932301a
Sanika7431 <sanikabhor769@gmail.com>
2025-07-31 12:24:10 +0530

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,39 @@
+package greek;
+
+import java.util.Scanner;
+
+public class Percentage {
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enetr the Percentage:");
+		int per = sc.nextInt();
+		
+		if(per<=60)
+		{
+			System.out.println("Grade:F");
+		}
+		else if(per<=70)
+		{
+			System.out.println("Grade:D");
+		}
+		else if(per<=80)
+		{
+			System.out.println("Grade:C");
+		}
+		else if(per<=90)
+		{
+			System.out.println("Grade:B");
+		}
+		else if(per<=100)
+		{
+			System.out.println("Grade:A");
+		}
+		else
+		{
+			System.out.println("Grade:F");
+		}
+		sc.close();
+	}
+}
