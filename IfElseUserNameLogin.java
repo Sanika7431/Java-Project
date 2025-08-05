de970ee0693fc872bb3cef01f436dc2afa835a2a
Sanika7431 <sanikabhor769@gmail.com>
2025-08-05 21:43:38 +0530

    Add files via upload

---
+++ IfElseUserNameLogin.java
@@ -0,0 +1,40 @@
+//Create a Java program that simulates a simple login system. Ask the user to enter a username and a password.
+
+package greek;
+
+import java.util.Scanner;
+
+public class IfElseUserNameLogin {
+
+	public static void main(String[] args)
+	{
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enetr Username:");
+		String username= sc.next();
+		System.out.println("Enter Password:");
+		int password = sc.nextInt();
+		
+		int Password=123456;
+		String Username ="admin";
+		
+		if(username.equals("admin"))// for  string .equal and for int ==
+		{
+			if(password==123456)
+			{
+				System.out.println("Login Successfully");
+			}
+		    else 
+		    	{
+		    	System.out.println("Incorrect Password");
+		    	}
+		}
+		else
+			{
+				System.out.println("Incorrect Username");
+				System.out.println("Login Unsuccessful. Please Try Again...");
+			}
+	    sc.close();
+	}
+}
+	
