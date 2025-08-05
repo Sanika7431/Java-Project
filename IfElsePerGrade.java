52ce10ae3f75a5788e8a3a4b5805826d2c5cfc00
Sanika7431 <sanikabhor769@gmail.com>
2025-08-05 21:47:39 +0530

    Add files via upload

---
+++ IfElsePerGrade.java
@@ -0,0 +1,42 @@
+//Create a Java program that simulates a simple login system. Ask the user to enter a username and a password.
+
+
+package greek;
+
+import java.util.Scanner;
+
+public class IfElsePerGrade {
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enetr the Percentage:");
+		int per = sc.nextInt();
+		
+		if(per<=60)
+		{
+			System.out.println("Grade:F");
+		}
+		else if(per<=70)
+		{
+			System.out.println("Grade:D");
+		}
+		else if(per<=80)
+		{
+			System.out.println("Grade:C");
+		}
+		else if(per<=90)
+		{
+			System.out.println("Grade:B");
+		}
+		else if(per<=100)
+		{
+			System.out.println("Grade:A");
+		}
+		else
+		{
+			System.out.println("Grade:F");
+		}
+		sc.close();
+	}
+}
