d6c30d2b9c7e57fc5da673c7162944516e247e4e
Sanika7431 <sanikabhor769@gmail.com>
2025-08-05 22:03:59 +0530

    Add files via upload

---
+++ CircleAreaUsingFinal.java
@@ -0,0 +1,26 @@
+package greek;
+
+import java.util.Scanner;
+
+public class CircleAreaUsingFinal {
+
+	public static void main(String[] args)
+	{
+		final float PI = 3.14f;
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter the radius:");
+		float radius = s.nextFloat();
+		System.out.println("Radius of Circle :" + radius);
+		float area = PI * radius * radius;
+		System.out.println("Area of Circle:" + area);
+		
+		System.out.println("");
+		System.out.println("Enter the base:");
+		float base = s.nextFloat();
+		System.out.println("Enter the height:");
+		float height = s.nextFloat();
+		float Tarea = 0.5f*base*height;
+		System.out.println("Area of Triangle: " + Tarea);
+		s.close();
+	}
+}
