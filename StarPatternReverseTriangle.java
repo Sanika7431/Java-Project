a017c299f664871be482371e1b2c9037cffec09f
Sanika7431 <sanikabhor769@gmail.com>
2025-08-05 21:13:04 +0530

    Add files via upload

---
+++ StarPatternReverseTriangle.java
@@ -0,0 +1,17 @@
+package greek;
+
+public class StarPatternReverseTriangle {
+
+	public static void main(String[] args)
+	{
+		int n=5;
+		for(int i=5;i>=1;i--)
+		{
+		    for(int j=1;j<=i;j++)
+		    {
+		    	System.out.printf("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
---
+++ StarPatternRightTriangle.java
@@ -0,0 +1,18 @@
+package greek;
+
+public class StarPatternRightTriangle {
+
+	public static void main(String[] args)
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)//row
+		{
+			for(int j=1;j<=i;j++)//column
+			{
+				System.out.printf("*");
+			}
+			
+			System.out.println();
+		}
+	}
+}
